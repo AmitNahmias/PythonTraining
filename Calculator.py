0c28d5e628ac1b27992ea43c09d718b444f6e491
Amit Nahmias <nahmias.amit@gmail.com>
2020-03-26 21:20:01 +0200

    Improvement of the first version
    Adding new design - after first CR, waiting for second.
    This one shorter and more efficient ( I hope ), for sure more generic.

--- Calculator.py
+++ Calculator.py
@@ -17,72 +17,50 @@ class math_funcs(object):
 
 
 class Calculator(object):
-    """The class defines special calculator """
+    """The class defines calculator with operations order"""
 
-    def __init__(self, list_of_exercise: str):
+    def __init__(self, list_of_exercise: list):
+        """
+        Defines five different dictionaries according specific math orders,
+        also getting a list that contains split string that represents math exercise.
+        """
         self.list_of_exercise = list_of_exercise
+        self.dict_of_operations_volume_5 = {"&": lambda num1, num2: min(num1, num2),
+                                            "$": lambda num1, num2: max(num1, num2),
+                                            "@": lambda num1, num2: math_funcs.average(num1, num2)}
+        self.dict_of_operations_volume_4 = {"!": lambda num: math_funcs.factorial(num),
+                                            "%": lambda num1, num2: num1 % num2}
+        self.dict_of_operations_volume_3 = {"^": lambda num1, num2: num1 ** num2}
+        self.dict_of_operations_volume_2 = {"*": lambda num1, num2: num1 * num2,
+                                            "/": lambda num1, num2: num1 / num2}
+        self.dict_of_operations_volume_1 = {"+": lambda num1, num2: num1 + num2,
+                                            "-": lambda num1, num2: num1 - num2}
+        self.list_of_dicts = [self.dict_of_operations_volume_5, self.dict_of_operations_volume_4,
+                              self.dict_of_operations_volume_3, self.dict_of_operations_volume_2,
+                              self.dict_of_operations_volume_1]
 
-    def scanner(self):
-        """The method scan for the most powerful sign"""
-        if "&" in self.list_of_exercise:
-            index = self.list_of_exercise.index('&')
-            result = min(self.list_of_exercise[index - 1], self.list_of_exercise[index + 1])
-            self.list_of_exercise[index] = result
-            self.list_of_exercise.pop(index + 1)
-            self.list_of_exercise.pop(index - 1)
-        elif "$" in self.list_of_exercise:
-            index = self.list_of_exercise.index('$')
-            result = max(self.list_of_exercise[index - 1], self.list_of_exercise[index + 1])
-            self.list_of_exercise[index] = result
-            self.list_of_exercise.pop(index + 1)
-            self.list_of_exercise.pop(index - 1)
-        elif "@" in self.list_of_exercise:
-            index = self.list_of_exercise.index('@')
-            result = math_funcs.average(int(self.list_of_exercise[index + 1]), int(self.list_of_exercise[index - 1]))
-            self.list_of_exercise[index] = result
-            self.list_of_exercise.pop(index + 1)
-            self.list_of_exercise.pop(index - 1)
-        elif "!" in self.list_of_exercise:
-            index = self.list_of_exercise.index('!')
-            result = math_funcs.factorial(self.list_of_exercise[index - 1])
-            self.list_of_exercise[index] = result
-            self.list_of_exercise.pop(index - 1)
-        elif "%" in self.list_of_exercise:
-            index = self.list_of_exercise.index('%')
-            result = int(self.list_of_exercise[index - 1]) % int(self.list_of_exercise[index + 1])
-            self.list_of_exercise[index] = result
-            self.list_of_exercise.pop(index + 1)
-            self.list_of_exercise.pop(index - 1)
-        elif "^" in self.list_of_exercise:
-            index = self.list_of_exercise.index('^')
-            result = int(self.list_of_exercise[index - 1]) ** int(self.list_of_exercise[index + 1])
-            self.list_of_exercise[index] = result
-            self.list_of_exercise.pop(index + 1)
-            self.list_of_exercise.pop(index - 1)
-        elif "*" in self.list_of_exercise:
-            index = self.list_of_exercise.index('*')
-            result = int(self.list_of_exercise[index - 1]) * int(self.list_of_exercise[index + 1])
-            self.list_of_exercise[index] = result
-            self.list_of_exercise.pop(index + 1)
-            self.list_of_exercise.pop(index - 1)
-        elif "/" in self.list_of_exercise:
-            index = self.list_of_exercise.index('/')
-            result = int(self.list_of_exercise[index - 1]) / int(self.list_of_exercise[index + 1])
-            self.list_of_exercise[index] = result
-            self.list_of_exercise.pop(index + 1)
-            self.list_of_exercise.pop(index - 1)
-        elif "+" in self.list_of_exercise:
-            index = self.list_of_exercise.index('+')
-            result = int(self.list_of_exercise[index - 1]) + int(self.list_of_exercise[index + 1])
-            self.list_of_exercise[index] = result
-            self.list_of_exercise.pop(index + 1)
-            self.list_of_exercise.pop(index - 1)
-        elif "-" in self.list_of_exercise:
-            index = self.list_of_exercise.index('-')
-            result = int(self.list_of_exercise[index - 1]) - int(self.list_of_exercise[index + 1])
-            self.list_of_exercise[index] = result
-            self.list_of_exercise.pop(index + 1)
-            self.list_of_exercise.pop(index - 1)
+    def change_list(self, result, index: int):
+        """The function changing list values after calculate"""
+        self.list_of_exercise[index] = result
+        self.list_of_exercise.pop(index + 1)
+        self.list_of_exercise.pop(index - 1)
+
+    def scan_and_calculate(self):
+        """The function scan the exercise and calculating him according math order"""
+        for dictionary in self.list_of_dicts:
+            i = 0
+            while i < len(self.list_of_exercise):
+                sign = self.list_of_exercise[i]
+                if sign == "!":
+                    result = dictionary[sign](int(self.list_of_exercise[self.list_of_exercise.index(sign) - 1]))
+                    self.change_list(result, self.list_of_exercise.index(sign))
+                    i -= 1
+                elif sign in dictionary.keys():
+                    result = dictionary[sign](int(self.list_of_exercise[self.list_of_exercise.index(sign) - 1]),
+                                              int(self.list_of_exercise[self.list_of_exercise.index(sign) + 1]))
+                    self.change_list(result, self.list_of_exercise.index(sign))
+                    i -= 2
+                i += 1
 
 
 def main():
@@ -90,7 +68,7 @@ def main():
     lst_from_exercise = exercise.split()
     my_instance = Calculator(lst_from_exercise)
     while len(lst_from_exercise) > 1:
-        my_instance.scanner()
+        my_instance.scan_and_calculate()
     print(lst_from_exercise[0])
 
 
