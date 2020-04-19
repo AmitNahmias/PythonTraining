9a5bda51028442fe4f9ff374fee01a7748d22405
Amit Nahmias <nahmias.amit@gmail.com>
2020-04-19 20:32:38 +0300

    Sunday - 19.04.2020
    New version in develop

--- room.py
+++ room.py
@@ -8,31 +8,36 @@ BUFFER_SIZE = 1024
 class Room(object):
     """The class defines a room"""
 
-    def __init__(self, port_from_user: str, user: Client):
+    def __init__(self, room_socket: socket, room_id: int):
         """
         This method initiate room instance.
-        :param port_from_user: getting port from user and open room in this port :str
-        :param user: getting the user that opened the room: Client
+        @param room_socket: represents the room socket: socket
         """
-        self.port_from_user = port_from_user
-        self.room_id = port_from_user
+        self.room_id = room_id
+        self.room_socket = room_socket
         self.client_dict = {}
-        self.room = Server(int(self.port_from_user))
-        self.user = user
 
-    def login(self):
-        """ This method getting user name from client and map it to client's dictionary according
-        the connection socket """
-        connection, address = self.room.server_socket.accept()
-        connection.send(bytes("Hello, what is your name?".encode()))
-        user_name = connection.recv(BUFFER_SIZE).decode()
+    def login_client_to_room(self):
+        # TODO: run in thread from the server script inside login_to_chat method
+        """
+        Accepting client to room and announces to all the room users
+        @return:
+        """
+        connection, address = self.room_socket.accept()
+        user_name = connection.recv(BUFFER_SIZE)
+        for conn in self.client_dict:
+            try:
+                conn.send(bytes("{} joined to the room".format(user_name)))
+            except ConnectionResetError or ConnectionAbortedError:
+                self.client_dict.pop(conn)
         self.client_dict[connection] = user_name
+        self.forward_msg_from_client_to_all_room(user_name, connection)
 
     def recv_msg(self, connection: socket):
         """
         The method receives message from client.
-        :param connection: the connected socket to the client :socket
-        :return: the message and the connected socket.
+        @param connection: the connected socket to the client :socket
+        @return: the message and the connected socket.
         """
         data_to_send = connection.recv(BUFFER_SIZE)
         return data_to_send, connection
@@ -42,8 +47,8 @@ class Room(object):
         """
         The method forwarding the message from one client to all the other clients in the room,
         if user has been disconnecting the method will remove him from the client dictionary.
-        :param data_from_client: the message that the client sent :bytes
-        :param connection_to_user_that_send_the_msg: the client who sent the message socket's
+        @param data_from_client: the message that the client sent :bytes
+        @param connection_to_user_that_send_the_msg: the client who sent the message socket's
         """
         for conn in self.client_dict:
             if conn != connection_to_user_that_send_the_msg:
--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-import socket
+import socket, threading
+from room import Room
 
 BUFFER_SIZE = 1024
 
@@ -6,14 +7,13 @@ BUFFER_SIZE = 1024
 class Server(object):
     """This class defines server instance"""
 
-    def __init__(self, port: int):
+    def __init__(self):
         """
-        The definition of server is to open port on local host
-        :param port: the port to open the server :str
+        The definition of server is to open port on local host and port 2510
         """
         self.ip = "127.0.0.1"
         self.rooms_list = []
-        self.port = port
+        self.port = 2510
         self.server_socket = socket.socket()
 
     def open_server(self):
@@ -29,10 +29,39 @@ class Server(object):
         self.rooms_list.remove(str(self.port))
         print("Server closed!")
 
+    def create_room(self, port: int):
+        """
+        Creates new room
+        @param port: the port that the room be connected to :int
+        @return:
+        """
+        room_instance = Room(socket.socket(), port)
+        room_socket = room_instance.room_socket
+        room_socket.bind((self.ip, port))
+        room_socket.listen()
+        self.rooms_list.append(port)
+        return room_instance
 
-    def open_rooms(self):
+    def login_to_chat(self):
         """
-        Show's the open room according to client's request
-        @return: rooms list : list
+        Login method - represents the login menu to the client
+        @return:
         """
-        return self.rooms_list
+        connection, address = self.server_socket.accept()
+        user_name = connection.recv(BUFFER_SIZE).decode()
+        connection.send(bytes(
+            'Hello {},\nTo view all opened rooms press 1, to create room press 2.'.format(user_name).encode()))
+        action = connection.recv(BUFFER_SIZE).decode()
+        try:
+            if action == "1":
+                connection.send(bytes(self.rooms_list))
+            elif action == "2":
+                connection.send(bytes("Choose port to open room".encode()))
+                chosen_port = connection.recv(BUFFER_SIZE).decode()
+                room_instance = self.create_room(int(chosen_port))
+                connection.send(bytes("You opened room in port {}".format(chosen_port).encode()))
+                #login_client_to_room_method_thread=threading.Thread(target=room_instance.login_client_to_room())
+
+
+        except IOError:
+            connection.send(bytes("Wrong action".encode()))
